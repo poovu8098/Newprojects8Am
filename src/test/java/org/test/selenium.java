764861d463feaf0fb1937d7883b1db4422240cbc
poovu8098 <pooarasan745@gmail.com>
2022-12-23 17:06:08 +0530

    poo cindition

--- src/test/java/org/test/selenium.java
+++ src/test/java/org/test/selenium.java
@@ -1,9 +1,16 @@
 package org.test;
 
+import org.testng.annotations.Test;
+
 public class selenium {
 	private void ghf() {
 		// TODO Auto-generated method stub
 System.out.println("gjjkhkgdfsdkl");
 
 	}
-}
+	@Test
+	private void task() {
+		// TODO Auto-generated method stub
+		System.out.println("poovu");
+	}
+	}
